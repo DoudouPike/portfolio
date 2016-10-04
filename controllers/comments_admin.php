d654a7c3dc0b9693e676d246571565ddf3c3b179
DoudouPike <doudoupike@hotmail.fr>
2016-10-04 18:30:24 +0200

    start project

--- controllers/comments_admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-if((isset($_SESSION["user"]) && $_SESSION["user"] === $comment->getUser()->getId()) || (isset($_SESSION["admin"]) && $_SESSION["admin"] === "1"))
-{
-require("views/comments_admin.phtml");
-}
-
-?>
--- controllers/create_comments.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_SESSION["id"]))
-{
-	require("views/create_comments.phtml");
-}
-?>
