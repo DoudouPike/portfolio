d3f0886460f06d3124b14b9e930b6a74b35d1a97
DoudouPike <doudoupike@hotmail.fr>
2016-10-04 17:37:15 +0200

    start project - support

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-<?php require("apps/skel.php"); ?>
+<?php
+
+	session_start();
+	$db = mysqli_connect("localhost", "root", "toQwEu192", "portfolio");
+	
+	$empty = "";
+	function __autoload($className)
+	{
+		require('models/'.$className.'.class.php');
+	}
+	
+	$error = '';
+	$page = "home";
+	$access = ["about", "register", "login", "portfolio", "mine"];
+	$accessIn = ["about", "contact", "portfolio", "mine", "logout", "dashboard"];
+	$accessAdmin = ["about", "contact", "portfolio", "mine", "logout", "dashboard"];
+	
+	if(isset($_GET['page']))
+	{
+		if(isset($_SESSION['id']))
+		{
+			if(isset($_SESSION['admin']) && ($_SESSION['admin'] == "1") && in_array($_GET['page'], $accessAdmin))
+			{
+				$page = $_GET['page'];
+			}
+			else if (in_array($_GET['page'], $accessIn))
+			{
+				$page = $_GET['page'];
+			}
+		}
+		else if(in_array($_GET['page'], $access))
+		{
+			$page = $_GET['page'];
+		}
+	}
+	
+	$traitementList = [
+		"register" => "users", "login" => "users", "logout" => "users",
+		"create_product" => "product", "edit_product" => "product", "remove_product" => "product",
+		"product_single" => "comments",
+		"create_category" => "category", "products" => "category",
+		"create_facture" => "facture", "panier" => "facture"
+	];
+	
+	if(isset($_GET['page'], $traitementList[$_GET['page']]))
+		require("controllers/traitement_".$traitementList[$_GET['page']].".php");
+	if (isset($_GET['ajax']))
+		require('controllers/recherche_res.php');
+	else
+		require('controllers/skel.php');
+?>
