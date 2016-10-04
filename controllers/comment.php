120e3c436e511f224e3702a12bdfd5d12b7e086a
DoudouPike <doudoupike@hotmail.fr>
2016-10-04 18:30:42 +0200

    start project

---
+++ controllers/comment.php
@@ -0,0 +1,16 @@
+<?php
+$commentManager = new CommentManager($db);
+$list = $commentManager->findByProduct($product);
+if($list)
+{
+	for ($i=0; $i < sizeof($list) ; $i++) { 
+		$comments = $list[$i];
+		require("views/comments.phtml");
+	}
+}
+else
+{
+	$empty = "Aucun commentaires pour ce produit.";
+	require("views/empty.phtml");
+}
+?>
