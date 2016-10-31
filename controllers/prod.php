1b4b669c2566046d399a48eb1dfe0463dece151b
DoudouPike <doudoupike@hotmail.fr>
2016-10-31 12:18:28 +0100

    en cours

--- controllers/prod.php
+++ controllers/prod.php
@@ -1,3 +1,13 @@
 <?php
-require('views/prod.phtml');
+if(isset($_GET['id']))
+{
+	$prodManager = new ProdManager($db);
+	$prod = $prodManager->findById($_GET['id']);
+	require('views/prod.phtml');
+}
+else
+{
+	$empty = "Cliquez sur la réalisation de votre choix";
+	require('views/empty.phtml');
+}
 ?>
