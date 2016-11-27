8a0c06ba53ac8cfb711505c6019342a19698ac70
DoudouPike <doudoupike@hotmail.fr>
2016-11-27 01:21:16 +0100

    en cours

--- index.php
+++ index.php
@@ -29,7 +29,6 @@ if(isset($_GET['admin'], $_SESSION['admin']) )
 $access = ["about", "contact", "login", "mine", "portfolio", "register",];
 $accessIn = ["about", "contact", "dashboard", "delete_user", "logout", "mine", "portfolio",];
 $accessAdmin = ["about", "comments", "contact", "create_comment", "create_note", "create_prod", "create_project", "create_review", "dashboard", "delete_comment", "delete_note", "delete_prod", "delete_project", "delete_review", "delete_user", "delete_user_admin", "edit_note", "edit_prod", "edit_project", "edit_review", "edit_user", "home_back", "logout", "mine", "notes_back", "portfolio", "prods", "prods_admin", "projects", "projects_admin", "reviews", "users"];
-
 if(isset($_GET['page']))
 {
 	if(isset($_SESSION['id']))
@@ -58,7 +57,6 @@ $traitementList = [
 	"create_project"=>"projects", "delete_project"=>"projects", "edit_project"=>"projects",
 	"create_review"=>"reviews", "delete_review"=>"reviews", "edit_review"=>"reviews"
 ];
-
 if(isset($_GET['page'], $traitementList[$_GET['page']]))
 	require("controllers/traitement_".$traitementList[$_GET['page']].".php");
 // if (isset($_GET['ajax']))
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -15,7 +15,68 @@ $(document).ready(function()
 	  		loginPanel.fadeOut();
 		}
 	});
+
+
+	// var canvas = $("#chart").get(0);
+	// if(canvas.getContext)
+	// {
+	//  	var ctx = canvas.getContext('2d');
+	// 	var lastend = 0;
+	// 	var data = [50, 50, 50, 50, 20];
+	// 	var myTotal = 0;
+	// 	var myColor = ['rgb(20,20,20)', 'rgb(70,70,70)', 'rgb(120,120,120)', 'rgb(170,170,170)', 'rgb(230,230,230)'];
+
+	// 	for (var e = 0; e < data.length; e++) {
+	// 	  myTotal += data[e];
+	// 	}
+
+	// 	for (var i = 0; i < data.length; i++) {
+	// 	  ctx.fillStyle = myColor[i];
+	// 	  ctx.beginPath();
+	// 	  ctx.moveTo(canvas.width / 2, canvas.height / 2);
+	// 	  // Arc Parameters: x, y, radius, startingAngle (radians), endingAngle (radians), antiClockwise (boolean)
+	// 	  ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2, lastend, lastend + (Math.PI * 2 * (data[i] / myTotal)), false);
+	// 	  ctx.lineTo(canvas.width / 2, canvas.height / 2);
+	// 	  ctx.fill();
+	// 	  lastend += Math.PI * 2 * (data[i] / myTotal);
+	// 	}
+	// }
 	
+	// var canvas = $("#chart").get(0);
+	// if(canvas.getContext)
+	// {
+	// 	var ctx = canvas.getContext('2d');
+	// 	//Dessin
+	// 	var myChart = new Chart(canvas, {
+	// 	    type: 'pie',
+	// 	    data: {
+	// 	        labels: [
+	// 	        	"HTML",
+	// 	        	"CSS",
+	// 	        	"PHP",
+	// 	        	"MySQL",
+	// 	        	"JS/jQuery"
+	// 	        ],
+	// 	        datasets: [{
+	// 	            data: [5, 5, 5, 5, 5],
+	// 	            backgroundColor: [
+	// 	                'rgb(20 ,20, 20)',
+	// 	                'rgb(70 ,70 ,70)',
+	// 	                'rgb(120, 120, 120)',
+	// 	                'rgb(170, 170, 170)',
+	// 	                'rgb(230, 230, 230)'
+	// 	            ]
+	// 	        }]
+	// 	    },
+	// 	});
+		
+		
+	// }
+	// else
+	// {
+		
+	// }
+
 	if($('#skillBox').is(':visible'))
 	{
 		$('#outline').delay("500").fadeIn();
